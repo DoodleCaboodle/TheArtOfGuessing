db21a8076b419e28b0c8a9132428259b2a8b8281
andu0987 <andrei_anculete@yahoo.ca>
2018-03-10 19:21:02 -0500

    colour change added

--- static/js/index.js
+++ static/js/index.js
@@ -4,17 +4,21 @@
 	var socket = io();
 	var canvas = document.getElementById("myCanvas");
 	var context = canvas.getContext("2d");
+	var colourPanel = document.getElementById("colourPanel");
 
 	//drawing flag
 	var drawing = false;
 
 	//current location
-	var curr = {};
+	var curr = {
+		colour: "#000000"
+	};
 
-	function drawLine(fromx, fromy, tox, toy, emit) {
+	function drawLine(fromx, fromy, tox, toy, colour, emit) {
 		context.beginPath();
 		context.moveTo(fromx, fromy);
 		context.lineTo(tox, toy);
+		context.strokeStyle = colour;
 		context.stroke();
 		context.closePath();
 
@@ -23,10 +27,21 @@
 				fromx: fromx / canvas.width,
 				fromy: fromy / canvas.height,
 				tox: tox / canvas.width,
-				toy: toy / canvas.height
+				toy: toy / canvas.height,
+				colour: colour
 			});
 		}
 	}
+
+	colourPanel.addEventListener("input", function(e){
+		curr.colour = e.target.value;
+	});
+
+	colourPanel.addEventListener("change", function(e){
+		curr.colour = e.target.value;
+	});
+
+	colourPanel.select();
 
 	//pen on paper
 	canvas.addEventListener('mousedown', function(e){
@@ -39,7 +54,14 @@
 	canvas.addEventListener('mouseup', function(e){
 		if (drawing) {
 			drawing = false;
-			drawLine(curr.x, curr.y, e.clientX, e.clientY, true);
+			drawLine(curr.x, curr.y, e.clientX, e.clientY, curr.colour, true);
+		}
+	});
+
+	canvas.addEventListener('mouseout', function(e){
+		if (drawing) {
+			drawing = false;
+			drawLine(curr.x, curr.y, e.clientX, e.clientY, curr.colour, true);
 		}
 	});
 
@@ -49,7 +71,7 @@
 	canvas.addEventListener('mousemove', function(e){
 		if ((Date.now() - lastEmit) >= 10) {
 			if(drawing) {
-				drawLine(curr.x, curr.y, e.clientX, e.clientY, true);
+				drawLine(curr.x, curr.y, e.clientX, e.clientY, curr.colour, true);
 				lastEmit = Date.now();
 				curr.x = e.clientX;
 				curr.y = e.clientY;
@@ -58,7 +80,7 @@
 	});
 
 	socket.on('drawing', function(data){
-		drawLine(data.fromx*canvas.width, data.fromy*canvas.height, data.tox*canvas.width, data.toy*canvas.height, false);
+		drawLine(data.fromx*canvas.width, data.fromy*canvas.height, data.tox*canvas.width, data.toy*canvas.height, data.colour, false);
 	});
 
 	window.addEventListener('resize', onResize);
