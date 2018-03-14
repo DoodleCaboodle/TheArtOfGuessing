dac28f8c3f47d1d07a0e5c311dc91df40103bc9e
Kalindu Kavishka De Costa <decostak@IITS-I406-05.utsc-labs.utoronto.ca>
2018-03-14 14:55:52 -0400

    server init

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 var io;
 var socket;
+var gameRoom = 'room1';
 
 //https://github.com/ericterpstra/anagrammatix/blob/master/agxgame.js
 exports.init = function(hio, hsocket) {
     io = hio;
     socket = hsocket;
     socket.emit('connected', {msg: 'You have connected!'});
+
+    socket.on('drawing', function(data){
+		socket.broadcast.emit('drawing', data);
+	});
     
+    socket.on('clear', function(data) {
+		socket.broadcast.emit('clear', data);
+    });
+
+    socket.on('ready', function(data) {
+        socket.join(gameRoom);
+    });
+
+    socket.on('message', function(data) {
+        socket.broadcast.emit('message', data);
+    });
+
 }
