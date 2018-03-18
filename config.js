5c6cc306de0752989f555ad243d16f7e098a5d43
Kalindu Kavishka De Costa <decostak@IITS-B473-09.utsc-labs.utoronto.ca>
2018-03-18 13:15:23 -0400

    complete merge

--- config.js
+++
@@ -1,9 +0,0 @@
-var filepath = __dirname + "/static/";
-var uri = "mongodb+srv://user-01:user-01@art-of-guessing-gdqip.mongodb.net/art-of-guessing";
-
-module.exports = {
-
-    filepath: filepath,
-    uri: uri
-
-};
