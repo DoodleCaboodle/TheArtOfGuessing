b7bcc1d81a1f3278a7fb0477337ec37d917d399c
andu0987 <andrei_anculete@yahoo.ca>
2018-03-27 20:01:02 -0400

    speech control implemented

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,139 +1,176 @@
 (function(){
 	"use strict";
 
-	var user = api.getCurrentUser();
+	window.onload = function() {
 
-	var h = document.createElement("H1");
-	var t = document.createTextNode(user.split('%40')[0]);
-	h.appendChild(t);
-	document.getElementById("mainInfo").appendChild(h);
+		var user = api.getCurrentUser();
 
-	api.getStats(user, function(err, res){
-		if (err) console.log(err)
-		var gamesWon = res.gamesWon;
-		var gamesLost = res.gamesPlayed - res.gamesWon;
-		var gameStatsConfig = {
-			type:'doughnut',
-			data: {
+		var h = document.createElement("H1");
+		var t = document.createTextNode(user.split('%40')[0]);
+		h.appendChild(t);
+		document.getElementById("mainInfo").appendChild(h);
+
+		api.getStats(user, function(err, res){
+			if (err) console.log(err)
+			var gamesWon = res.gamesWon;
+			var gamesLost = res.gamesPlayed - res.gamesWon;
+			var gameStatsConfig = {
+				type:'doughnut',
+				data: {
+					datasets: [{
+						data: [
+							gamesWon,
+							gamesLost
+					],
+					backgroundColor: [
+							"#46BFBD",
+							"#F7464A"
+					],
+					label: 'Game Stats'
+					}],
+					labels: [
+						"Wins",
+						"Loses"
+					]
+				},
+				options: {
+					responsive: true,
+					legend: {
+						position: 'top',
+					},
+					title: {
+						display: true,
+						text: 'Games'
+					},
+					animation: {
+						animateScale: true,
+						animateRotate: true
+					}
+				}
+			}
+			var ctx = document.getElementById("gameStats").getContext("2d");
+			ctx.canvas.width = 180;
+			ctx.canvas.height = 180;
+			window.myDoughnut = new Chart(ctx, gameStatsConfig);
+
+			var wordsDict = res.words;
+
+			var theWords = Object.keys(wordsDict);
+
+			var tempVals = Object.values(wordsDict);
+			var drawn = [];
+			var guessed = [];
+			for(var i = 0; i < tempVals.length; i++) {
+				drawn.push(tempVals[i].drawn);
+				guessed.push(tempVals[i].guessed);
+			}
+
+			var barWordsData = {
+				labels: theWords,
 				datasets: [{
-					data: [
-						gamesWon,
-						gamesLost
-				],
-				backgroundColor: [
-						"#46BFBD",
-						"#F7464A"
-				],
-				label: 'Game Stats'
-				}],
-				labels: [
-					"Wins",
-					"Loses"
-				]
-			},
-			options: {
-				responsive: true,
-				legend: {
-					position: 'top',
+					label: 'Drawn',
+					backgroundColor: "red",
+					borderColor: "#8B0000",
+					borderWidth: 1,
+					data: drawn
+				}, {
+					label: 'Guessed',
+					backgroundColor: "#87CEFA",
+					borderColor: "#0000FF",
+					borderWidth: 1,
+					data: guessed
+				}]
+			}
+			var ctx = document.getElementById("wordsData").getContext("2d");
+			window.myBarChart = new Chart(ctx, {
+				type: 'bar',
+				data: barWordsData,
+				options: {
+					responsive: true,
+					legend: {
+						position: 'top'
+					},
+					title: {
+						display: true,
+						text: 'Words'
+					}
+				}
+			});
+
+			var roundsWon = res.roundsWon;
+			var roundsLost = res.roundsPlayed - res.roundsWon;
+			var roundsStatsConfig = {
+				type:'doughnut',
+				data: {
+					datasets: [{
+						data: [
+							roundsWon,
+							roundsLost
+					],
+					backgroundColor: [
+							"#46BFBD",
+							"#F7464A"
+					],
+					label: 'Rounds Stats'
+					}],
+					labels: [
+						"Wins",
+						"Loses"
+					]
 				},
-				title: {
-					display: true,
-					text: 'Games'
-				},
-				animation: {
-					animateScale: true,
-					animateRotate: true
+				options: {
+					responsive: true,
+					legend: {
+						position: 'top',
+					},
+					title: {
+						display: true,
+						text: 'Rounds'
+					},
+					animation: {
+						animateScale: true,
+						animateRotate: true
+					}
 				}
 			}
-		}
-		var ctx = document.getElementById("gameStats").getContext("2d");
-		ctx.canvas.width = 180;
-		ctx.canvas.height = 180;
-		window.myDoughnut = new Chart(ctx, gameStatsConfig);
-
-		var wordsDict = res.words;
-
-		var theWords = Object.keys(wordsDict);
-
-		var tempVals = Object.values(wordsDict);
-		var drawn = [];
-		var guessed = [];
-		for(var i = 0; i < tempVals.length; i++) {
-			drawn.push(tempVals[i].drawn);
-			guessed.push(tempVals[i].guessed);
-		}
-
-		var barWordsData = {
-			labels: theWords,
-			datasets: [{
-				label: 'Drawn',
-				backgroundColor: "red",
-				borderColor: "#8B0000",
-				borderWidth: 1,
-				data: drawn
-			}, {
-				label: 'Guessed',
-				backgroundColor: "#87CEFA",
-				borderColor: "#0000FF",
-				borderWidth: 1,
-				data: guessed
-			}]
-		}
-		var ctx = document.getElementById("wordsData").getContext("2d");
-		window.myBarChart = new Chart(ctx, {
-			type: 'bar',
-			data: barWordsData,
-			options: {
-				responsive: true,
-				legend: {
-					position: 'top'
-				},
-				title: {
-					display: true,
-					text: 'Words'
-				}
-			}
+			var ctx = document.getElementById("roundsStats").getContext("2d");
+			ctx.canvas.width = 180;
+			ctx.canvas.height = 180;
+			window.myDoughnut = new Chart(ctx, roundsStatsConfig);
 		});
 
-		var roundsWon = res.roundsWon;
-		var roundsLost = res.roundsPlayed - res.roundsWon;
-		var roundsStatsConfig = {
-			type:'doughnut',
-			data: {
-				datasets: [{
-					data: [
-						roundsWon,
-						roundsLost
-				],
-				backgroundColor: [
-						"#46BFBD",
-						"#F7464A"
-				],
-				label: 'Rounds Stats'
-				}],
-				labels: [
-					"Wins",
-					"Loses"
-				]
-			},
-			options: {
-				responsive: true,
-				legend: {
-					position: 'top',
-				},
-				title: {
-					display: true,
-					text: 'Rounds'
-				},
-				animation: {
-					animateScale: true,
-					animateRotate: true
-				}
-			}
-		}
-		var ctx = document.getElementById("roundsStats").getContext("2d");
-		ctx.canvas.width = 180;
-		ctx.canvas.height = 180;
-		window.myDoughnut = new Chart(ctx, roundsStatsConfig);
-	});
+		var logoutFunc = function() {
+        	window.location.href = "/signout";
+        }
+
+        var homeFunc = function() {
+        	window.location.href = "/";
+        }
+
+        var backFunc = function() {
+        	window.history.back();
+        }
+
+        var nextFunc = function() {
+        	window.history.forward();
+        }
+
+		if (annyang) {
+        	var commands = {
+        		'logout': logoutFunc,
+        		'log out': logoutFunc,
+        		'sign out': logoutFunc,
+        		'signout': logoutFunc,
+        		'home': homeFunc,
+        		'back': backFunc,
+        		'next': nextFunc,
+        		'forward': nextFunc
+        	};
+
+        	annyang.addCommands(commands);
+        	annyang.start();
+        }
+
+	}
+
 }());
